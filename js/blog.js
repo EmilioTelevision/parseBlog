2b0e6b86ab6c1ac6ccb2b4a86d7cc10a65b24041
EmilioTelevision <emiliotelevision@gmail.com>
2014-11-20 14:35:39 -0500

    retrieve blog posts

--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,28 @@
-$(function() {
- 
-    Parse.$ = jQuery;
- 
-    Parse.initialize("rdoRfvEuXA8pGuDFHsPjnP4LdnluN4KM8ZOap07N", "qhhROpIuGhaNidCWbupvvECLjysQXPUoD76bjF6u");
- 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
- 
+Parse.initialize("rdoRfvEuXA8pGuDFHsPjnP4LdnluN4KM8ZOap07N", "qhhROpIuGhaNidCWbupvvECLjysQXPUoD76bjF6u");
+
+var Blog = Parse.Object.extend("Blog");
+
+var Blogs = Parse.Collection.extend({
+    model: Blog
+});
+
+var blogs = new Blogs();
+
+var BlogsView = Parse.View.extend({
+	template: Handlebars.compile($('#blogs-tpl').html()),
+	render: function() {
+		var collection = { blog: this.collection.toJSON() };
+		this.$el.html(this.template(collection));
+	}
+});
+
+blogs.fetch({
+    success: function(blogs) {
+        var blogsView = new BlogsView({ collection: blogs });
+	    blogsView.render();
+	    $('.main-container').html(blogsView.el);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
 });
